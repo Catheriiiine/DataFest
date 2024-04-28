a70629a950be310ff05420ca4ed676e4044963c8
youyang <youyang.guo@mail.utoronto.ca>
2024-04-27 20:19:22 -0400

    chapter groups

--- separate_behaviors.py
+++ separate_behaviors.py
@@ -18,14 +18,43 @@ filtered_data = grouped_data[grouped_data['engaged'] > 0]
 filtered_data['log_engaged'] = np.log10(filtered_data['engaged'])
 
 
-plt.figure(figsize=(10, 6))
-sns.histplot(filtered_data['log_engaged'], kde=True, binwidth=0.1)  # Adjusted bin width for log scale
-plt.title('Log-Scaled Distribution of Total Engagement Time for All Pages')
-plt.xlabel('Log10 of Engagement Time (ms)')
-plt.ylabel('Frequency')
-plt.grid(True)
-plt.show()
+# plt.figure(figsize=(10, 6))
+# sns.histplot(filtered_data['log_engaged'], kde=True, binwidth=0.1)  # Adjusted bin width for log scale
+# plt.title('Log-Scaled Distribution of Total Engagement Time for All Pages')
+# plt.xlabel('Log10 of Engagement Time (ms)')
+# plt.ylabel('Frequency')
+# plt.grid(True)
+# plt.show()
 
 percentile_33 = np.percentile(filtered_data['log_engaged'], 33)
 percentile_66 = np.percentile(filtered_data['log_engaged'], 66)
+
+def categorize_reading(log_engaged):
+    if log_engaged < percentile_33:
+        return 'Fast Reading'
+    elif log_engaged < percentile_66:
+        return 'Medium Reading'
+    else:
+        return 'Slow Reading'
+
+# Apply categorization function to the DataFrame
+filtered_data['reading_speed'] = filtered_data['log_engaged'].apply(categorize_reading)
+
+# Calculate average engagement time in milliseconds for each category
+average_times = filtered_data.groupby('reading_speed')['engaged'].mean().reset_index()
+
+# Group by student, book, chapter number, and calculate the total reading time per chapter for each student
+total_time_per_chapter = filtered_data.groupby(['student_id', 'book', 'chapter_number'])['engaged'].sum().reset_index()
+
+# Log-scale the total times
+total_time_per_chapter['log_total_engaged'] = np.log10(total_time_per_chapter['engaged'])
+
+# Plotting the distribution of these log-scaled total engagement times per chapter
+plt.figure(figsize=(10, 6))
+sns.histplot(total_time_per_chapter['log_total_engaged'], kde=True, binwidth=0.1)
+plt.title('Distribution of Log-Scaled Total Reading Time per Chapter')
+plt.xlabel('Log10 of Total Engagement Time per Chapter (ms)')
+plt.ylabel('Frequency')
+plt.grid(True)
+plt.show()
 
